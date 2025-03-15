8dc5d3900bd035afe00f2f47ff6f1b9def3da404
mofeo.ona <mofeo.ona@gmail.com>
2025-03-15 13:50:58 -0700

    python code practice

--- Untitled.py
+++ Untitled.py
@@ -1,1 +1,23 @@
-print("im coming home")
+#print("im coming home")
+
+#print('I a m a basketballer who frquently plays football')
+#print("I have an engineering project")
+#print("we're making an automata toy")
+print("$%&*@")
+
+print("""" 
+      
+my name is mofe
+      i am an engineering studebt 
+
+      that loves programming
+      i aslo love knittinf and draeing 
+
+      and i love amala and gbegiri and ewa agoyin
+      
+      """)
+
+print(20+2)
+print(5*7)
+print(30/3)
+print(9+8*7+6-4+8/2)
