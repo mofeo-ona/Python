08b92bf32efb180b41199a77a64b8088d92309c7
mofeo.ona <mofeo.ona@gmail.com>
2025-04-26 12:02:55 -0700

    Recipe Generator updated

--- Recipe Randomizer.py
+++ Recipe Randomizer.py
@@ -19,29 +19,60 @@ while x == True:
         sw=input()
 
         if sw == "donuts":
-            print("The ingredients for donuts are\n\nxxx\nxxx\nxxx\nxxx\nxxx")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
-        elif sw == "muffins":
-            print("""The ingredients for muffins are:
-                  2 cups all-purpose flour
-                  1/2 cup sugar
-                  1 tbsp baking powder
-                  1/2 tsp salt
-                  1 cup milk
-                  1/4 cup melted butter
-                  1 egg
+            print("""The ingredients for donuts are
+    1. 1 cup flour
+    2. 1/4 cup sugar
+    3. 1 tsp baking powder
+    4. pinch of salt
+    5. 1/4 cup milk
+    6. 1 egg
+    7. 1 tbsp oil
+
                   """)
             print("""The instructions are 
-                  Preheat oven to 375°F.
-                  Mix flour, sugar, baking powder
-                  Add milk, egg, melted butter
-                  Stir until just barely combined.
+    1. Mix dry ingredients
+    2. Mix wet ingredients
+    3. stir into a dough
+    4. Shape into rings
+    5. Fry until golden brown                  
                   
+                  """)
+        elif sw == "muffins":
+            print("""The ingredients for muffins are:
+    1. 2 cups all-purpose flour
+    2. 1/2 cup sugar
+    3. 1 tbsp baking powder
+    4. 1/2 tsp salt
+    5. 1 cup milk
+    6. 1/4 cup melted butter
+    7. 1 egg
+                  """)
+            print("""The instructions are 
+    1. Preheat oven to 375°F.
+    2. Mix flour, sugar, baking powder
+    3. Add milk, egg, melted butter
+    4. Stir until just barely combined.
+    5. Pour into muffin tin
+    6. Bake for 15-20 mins.
                   
                   """)
         elif sw == "cookies":
-            print("The ingredients for cookies are\n\nxxx\nxxx\nxxx\nxxx\nxxx\n")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for cookies are
+    1. Butter
+    2. Sugar
+    3. Eggs
+    4. Flour
+    5. Baking soda
+    6. Vanilla
+    7. Chocolate chips
+                  """)
+            print("""The instructions are
+    1.Mix butter, sugar, vanilla, eggs.
+    2. Add flour, Baking soda, stir well.
+    3. Fold in chocolate chips.
+    4. Scoop dough onto baking sheet.
+    5. Bake 10 minutes at 350°F.
+                  """)
         else:
             print("choose one of the options")
 
@@ -53,25 +84,50 @@ while x == True:
 
         if sp =="spicy chicken tenders":
             print("""The ingredients for spicy chicken tenders are
-              chicken wings
-              xxx
-              xxx
-              xxx
-              xxx
-              xxx
+    1. chicken tenders
+    2. 1/2 cup hot sauce
+    3. 1 tbsp butter
+    4. 1 tsp paprika
+    5. salt
+    6. oil
               
               Here are the instructions
-              1.
-              2.
-              3.
-              4.
+    1. Season chicken
+    2. Fry until golden
+    3. Melt butter with hot sauce
+    4. Toss chicken in spicy sauce
               """)
         elif sp == "spicy scrambled eggs":
-            print("The ingredients for spicy scrambled eggs are\n\nxxx\nxxx\nxxx\nxxx\nxxx")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for spicy scrambled eggs are
+    1. eggs
+    2. chilli flakes
+    3. salt & pepper
+    4. oil or butter
+                  
+                  """)
+            print("""The instructions are 
+    1. Whisk eggs with chilli flakes salt and pepper
+    2. Heat oil in pan
+    3. pour in eggs and scramble until cooked
+                  
+                  """)
         elif sp == "jalapeno fries":
-            print("The ingredients for jalapeno fries are\n\nxxx\nxxx\nxxx\nxxx\nxxx\n")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for jalapeno fries are
+    1. Jalapeños
+    2. Flour
+    3. Cornmeal
+    4. Salt, pepper
+    5. Buttermilk
+    6. Oil
+                  """)
+            print("""The instructions are 
+    1. Slice jalapeños into strips.
+    2. Soak in buttermilk briefly.
+    3. Mix flour, cornmeal, seasoning.
+    4. Coat jalapeños in dry mix.
+    5. Fry until golden brown.
+                  
+                  """)
         else:
             print("choose one of the options")
 
@@ -82,14 +138,58 @@ while x == True:
         sa=input().lower()
 
         if sa =="potato wedges":
-            print("The ingredients for potato wedges are\n\nxxx\nxxx\nxxx\nxxx\nxxx")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for potato wedges are
+   1. 2–3 large potatoes
+   2. 2 tbsp olive oil
+   3. 1 tsp paprika
+   4. 1/2 tsp garlic powder
+   5. Salt and pepper to taste
+                  
+                  """)
+            print("""The instructions are 
+    1. Cut potatoes into thick wedges.
+    2. Toss with oil and spices.
+    3. Spread wedges on baking sheet.
+    4. Bake 40 minutes at 425°F.
+    5. Flip halfway for even crisp.
+                  
+                  """)
         elif sa == "pretzels":
-            print("The ingredients for pretzels are\n\nxxx\nxxx\nxxx\nxxx\nxxx\n")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for pretzels are
+    1. Flour
+    2. Yeast
+    3. Warm water
+    4. Sugar
+    5. Salt
+    6. Baking soda
+    7. Egg
+    8. Coarse salt
+                  
+                  """)
+            print("""The instructions are
+    1. Mix yeast, water, sugar, salt.
+    2. Add flour, knead smooth dough.
+    3. Let rise, then shape pretzels.
+    4. Boil in baking soda water.
+    5. Brush egg, sprinkle coarse salt.
+    6. Bake until golden and puffy.
+                   """)
+            
         elif sa == "grilled cheese sandwich":
-            print("The ingredients for grilled cheese sandwich are\n\nxxx\nxxx\nxxx\nxxx\nxxx\n")
-            print("The instructions are \n\nxxx\nxxx\nxxx")
+            print("""The ingredients for grilled cheese sandwich are
+    1. Bread slices (2 per sandwich)
+    2. Butter or mayo
+    3. Cheese
+                  
+                  \n""")
+            print("""The instructions are 
+    1. Butter one side of bread.
+    2. Add cheese between unbuttered sides.
+    3. Grill buttered sides on pan.
+    4. Flip when golden and melty.
+    5. Slice, serve hot, enjoy life.
+                  
+                  """)
         else:
             print("choose one of the options")
 
@@ -114,6 +214,6 @@ while x == True:
     if quit == "yes":
         x=True
     elif quit == "no":
-        x=False
+         x=False
     else:
         print("Pick yes or no")
